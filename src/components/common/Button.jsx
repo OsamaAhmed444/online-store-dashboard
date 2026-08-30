df47b862647bae55730330367aa9dac00d2ee3c7
Basma <bsmtbdalawl@gmail.com>
2026-08-30 16:33:14 +0300

    Improve Button And Input Components

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
-export default function Button() {
+function Button({ type, onClick, className, disabled, loading, children, ...props }) {
+  const bestStyles = "px-4 py-2 text-white rounded-lg bg-slate-700 hover:bg-slate-800 focus:ring-1 focus:ring-slate-400 disabled:cursor-not-allowed disabled:opacity-50 transition-all duration-300"
   return (
-    <div>Button</div>
+    <button type={type ? type : "button"} onClick={onClick} className={`${bestStyles} ${className ? className : ""}  `} disabled={disabled || loading} {...props}>{loading ? "Loading..." : children}</button>
   )
 }
+
+export default Button;
