b62c38962b19814067a9697302ce557f85806c5c
Basma <bsmtbdalawl@gmail.com>
2026-08-31 18:38:17 +0300

    Update Button And Input Components

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
 
 function Button({ type, onClick, className, disabled, loading, children, ...props }) {
-  const bestStyles = "px-4 py-2 text-white rounded-lg bg-slate-700 hover:bg-slate-800 focus:ring-1 focus:ring-slate-400 disabled:cursor-not-allowed disabled:opacity-50 transition-all duration-300"
+  const bestStyles = "px-6 py-2 text-[#FFF7ED] rounded-lg bg-[#F97316] hover:bg-[#F8923C] focus:ring-1 focus:ring-[#FFEDD5] disabled:cursor-not-allowed disabled:opacity-50 transition-all duration-300 cursor-pointer capitalize"
   return (
     <button type={type ? type : "button"} onClick={onClick} className={`${bestStyles} ${className ? className : ""}  `} disabled={disabled || loading} {...props}>{loading ? "Loading..." : children}</button>
   )
--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
 
 export default function Input({ type, name, id, className, label, value, placeholder, onChange, error, ...props }) {
-  const bestStyles = "px-6 py-2 ring-1 rounded-lg focus:ring-slate-400 disabled:cursor-not-allowed transition-all duration-300";
+  const bestStyles = "px-6 py-2 ring-1 rounded-lg outline-none bg-[#FFF7ED] focus:ring-[#F97316] disabled:cursor-not-allowed transition-all duration-300";
 
   return (
-    <div className="flex flex-col justify-center items-center gap-5">
+    <div className="flex flex-col justify-start items-start gap-3 w-full">
 
-      <label htmlFor={id ? id : name}>{label}</label>
-      <input type={type} name={name} id={id ? id : name} className={`${bestStyles} ${className ? className : ""} ${error ? "ring-red-600" : "ring-slate-700"}`} value={value} placeholder={placeholder} onChange={onChange} {...props} />
-      <div className={`px-4 py-2 rounded-lg bg-white flex justify-center gap-2 ${error ? "flex" : "hidden"}`}>{error}</div>
+      <label htmlFor={id ? id : name} className="text-[#F97316] font-medium ">{label}</label>
+      <input type={type} name={name} id={id ? id : name} className={`${bestStyles} ${className ? className : ""} ${error ? "ring-red-600" : "ring-[#FFEDD5]"}`} value={value} placeholder={placeholder} onChange={onChange} {...props} />
+      <div className={`px-4 py-2 rounded-lg bg-white flex justify-center gap-2 absolute z-50 right-4 top-3 ring-1 ring-[#F97316] ${error ? "flex" : "hidden"}`}>{error}</div>
 
     </div>
   )
