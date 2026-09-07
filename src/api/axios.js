61cb950f2c2b2acaae674e57e3ff4a61468a9881
OsamaAhmed444 <osamaalnady00@gmail.com>
2026-09-07 16:01:54 +0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,26 +1,7 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: import.meta.env.DEV ? "/api" : import.meta.env.VITE_API_URL,
-//   withCredentials: true,
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = sessionStorage.getItem("token");
-
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   return config;
-// });
-
-// export default api;
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL: import.meta.env.DEV ? "/api" : import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
@@ -35,3 +16,5 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+
+ 
